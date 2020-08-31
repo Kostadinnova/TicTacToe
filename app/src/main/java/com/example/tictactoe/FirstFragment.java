a8cebc2886745f01d588efb46202e2556c01af21
Kostadinnova <51726348+Kostadinnova@users.noreply.github.com>
2020-09-01 00:30:33 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/tictactoe/FirstFragment.java
@@ -0,0 +1,14 @@
+package com.example.tictactoe;
+
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+
+    public FirstFragment() {
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/tictactoe/SecondActivity.java
@@ -0,0 +1,26 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+
+    public void MainActivity(View view) {
+
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
